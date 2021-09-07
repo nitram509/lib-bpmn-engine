b2e6788164473029d86fab4bdf000a823e15538b
nitram509 <maki@bitkings.de>
2021-09-07 21:43:34 +0200

    rewrite model code from hand ... works betterfor me

--- pgk/engine/engine.go
+++ pgk/engine/engine.go
@@ -1,12 +1,12 @@
 package engine
 
 import (
-	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20"
+	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20NEW"
 )
 
 type BpmnEngine interface {
 }
 
-func BpmnEngineFromDefinitions(definitions BPMN20.TDefinitions) BpmnEngine {
+func BpmnEngineFromDefinitions(definitions BPMN20NEW.TDefinitions) BpmnEngine {
 	return nil
 }
--- pgk/engine/engine_test.go
+++ pgk/engine/engine_test.go
@@ -1,14 +1,43 @@
 package engine
 
 import (
+	"encoding/xml"
+	"fmt"
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
-	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20"
+	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20NEW"
+	"io/ioutil"
 	"testing"
 )
 
+//func TestEngine(t *testing.T) {
+//
+//	xmldata, err := ioutil.ReadFile("../../test/simple_task.xml") // just pass the file name
+//	if err != nil {
+//		fmt.Print(err)
+//	}
+//
+//	var definitions BPMN20.TDefinitions
+//	err = xml.Unmarshal(xmldata, &definitions)
+//	fmt.Println(err)
+//
+//
+//	engine := BpmnEngineFromDefinitions(definitions)
+//
+//	then.AssertThat(t, engine, is.Not(is.Nil()))
+//}
+
 func TestEngine(t *testing.T) {
-	var definitions BPMN20.TDefinitions
+
+	xmldata, err := ioutil.ReadFile("../../test/simple_task.xml") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var definitions BPMN20NEW.TDefinitions
+	err = xml.Unmarshal(xmldata, &definitions)
+	fmt.Println(err)
+
 	engine := BpmnEngineFromDefinitions(definitions)
 
 	then.AssertThat(t, engine, is.Not(is.Nil()))
