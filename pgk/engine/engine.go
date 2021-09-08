de5c7655065a6388e7c280256ef6605c67206aa9
nitram509 <maki@bitkings.de>
2021-09-08 08:41:29 +0200

    implement simple registry

--- pgk/engine/engine.go
+++ pgk/engine/engine.go
@@ -1,12 +1,50 @@
 package engine
 
 import (
-	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20NEW"
+	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20"
 )
 
-type BpmnEngine interface {
+type Node struct {
+	Name string
+	Id   string
 }
 
-func BpmnEngineFromDefinitions(definitions BPMN20NEW.TDefinitions) BpmnEngine {
-	return nil
+type registeredProcess struct {
+	processInfo ProcessInfo
+}
+
+type BpmnEngine interface {
+	LoadFromDefinitions(definitions BPMN20.TDefinitions)
+}
+
+type BpmnEngineCore struct {
+	registry []registeredProcess
+}
+
+func (core BpmnEngineCore) LoadFromDefinitions(definitions BPMN20.TDefinitions) (DeployResult, error) {
+	info := ProcessInfo{
+		bpmnProcessId: "123",
+		version:       1,
+		processKey:    456,
+		resourceName:  "xxx",
+	}
+
+	core.registry = append(core.registry, registeredProcess{info})
+
+	result := DeployResult{}
+	result.key = "1234567890"
+	result.processes = append(result.processes, info)
+	return result, nil
+}
+
+type DeployResult struct {
+	key       string
+	processes []ProcessInfo
+}
+
+type ProcessInfo struct {
+	bpmnProcessId string
+	version       uint64
+	processKey    uint64
+	resourceName  string
 }
--- pgk/engine/engine_test.go
+++ pgk/engine/engine_test.go
@@ -5,40 +5,26 @@ import (
 	"fmt"
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
-	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20NEW"
+	"github.com/nitram509/golib-bpmn-model/pgk/spec/BPMN/20100501/BPMN20"
 	"io/ioutil"
 	"testing"
 )
 
-//func TestEngine(t *testing.T) {
-//
-//	xmldata, err := ioutil.ReadFile("../../test/simple_task.xml") // just pass the file name
-//	if err != nil {
-//		fmt.Print(err)
-//	}
-//
-//	var definitions BPMN20.TDefinitions
-//	err = xml.Unmarshal(xmldata, &definitions)
-//	fmt.Println(err)
-//
-//
-//	engine := BpmnEngineFromDefinitions(definitions)
-//
-//	then.AssertThat(t, engine, is.Not(is.Nil()))
-//}
-
-func TestEngine(t *testing.T) {
+func TestLoadFromBpmnDefinitions(t *testing.T) {
 
 	xmldata, err := ioutil.ReadFile("../../test/simple_task.xml") // just pass the file name
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	var definitions BPMN20NEW.TDefinitions
+	var definitions BPMN20.TDefinitions
 	err = xml.Unmarshal(xmldata, &definitions)
 	fmt.Println(err)
 
-	engine := BpmnEngineFromDefinitions(definitions)
+	core := BpmnEngineCore{}
+	deployResult, err := core.LoadFromDefinitions(definitions)
 
-	then.AssertThat(t, engine, is.Not(is.Nil()))
+	then.AssertThat(t, err, is.Nil())
+	then.AssertThat(t, deployResult.key, is.Not(is.Empty()))
+
 }
