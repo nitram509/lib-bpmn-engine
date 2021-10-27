14e025a07b650d7db494e48b938b0b5c19ea157a
stoovon <murraystuart@mac.com>
2021-10-27 21:00:40 +0100

    Enhance sample and add test
    - Add test to show variable context mutation.
    - Add enhanced handler generator to show how we might use this in the
      example.

--- pkg/bpmn_engine/example_test.go
+++ pkg/bpmn_engine/example_test.go
@@ -1,6 +1,9 @@
 package bpmn_engine_test
 
-import "github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine"
+import (
+	"fmt"
+	"github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine"
+)
 
 func ExampleNew() {
 	// basic example loading a BPMN from file,
@@ -9,10 +12,15 @@ func ExampleNew() {
 	bpmnEngine := bpmn_engine.New()
 	simpleTask := "simple_task"
 	bpmnEngine.LoadFromFile("test.bpmn.xml", simpleTask)
-	bpmnEngine.AddTaskHandler(simpleTask, "aTaskId", myHandler)
+	bpmnEngine.AddTaskHandler(simpleTask, "aTaskId", myHandlerGenerator(bpmnEngine))
 	bpmnEngine.Execute(simpleTask)
 }
 
-func myHandler(id string) {
-	println("Executing task id=" + id)
+func myHandlerGenerator(state bpmn_engine.BpmnEngineState) func(id string) {
+	return func(id string) {
+		println("Executing task id=" + id)
+
+		fmt.Printf("Variable context for task: %v",
+			state.GetProcesses("simple_task")[0].VariableContext)
+	}
 }
