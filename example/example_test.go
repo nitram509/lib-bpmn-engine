a8ecabd142376f03e402d151825505de05cdf84b
nitram509 <maki@bitkings.de>
2021-11-02 22:49:36 +0100

    cleanup example

--- example/example_test.go
+++ example/example_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 func ExampleNew() {
-	// basic example loading a BPMN from file,
-	// register a handler for a service task by Id
-	// and execute the process
+	// create a new named engine
 	bpmnEngine := bpmn_engine.New("a name")
+	// basic example loading a BPMN from file,
 	process, _ := bpmnEngine.LoadFromFile("test.bpmn.xml")
+	// register a handler for a service task by Id
 	bpmnEngine.AddTaskHandler("aTaskId", myHandlerGenerator(bpmnEngine))
+	// and execute the process
 	bpmnEngine.CreateAndRunInstance(process.ProcessKey)
 }
 
 func myHandlerGenerator(state bpmn_engine.BpmnEngineState) func(id string) {
 	return func(id string) {
 		println("Executing task id=" + id)
-
 		fmt.Printf("Variable context for task: %v",
 			state.GetProcessInstances()[0].VariableContext)
 	}
