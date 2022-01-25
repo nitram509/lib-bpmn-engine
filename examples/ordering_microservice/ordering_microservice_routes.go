a1dfa42c18c3be96d4c351ef12f034a706138e30
nitram509 <maki@bitkings.de>
2022-01-25 22:03:56 +0100

    update comments

--- examples/ordering_microservice/ordering_microservice_routes.go
+++ examples/ordering_microservice/ordering_microservice_routes.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 func initHttpRoutes() {
-	http.HandleFunc("/api/order", handleOrder)                                        // POST new Order
-	http.HandleFunc("/api/receive-payment", handleReceivePayment)                     // webhook from the payment system
-	http.HandleFunc("/show-process.html", handleShowProcess)                          // the index page
+	http.HandleFunc("/api/order", handleOrder)                                        // POST new or GET existing Order
+	http.HandleFunc("/api/receive-payment", handleReceivePayment)                     // webhook for the payment system
+	http.HandleFunc("/show-process.html", handleShowProcess)                          // shows the BPMN diagram
 	http.HandleFunc("/index.html", handleIndex)                                       // the index page
 	http.HandleFunc("/", handleIndex)                                                 // the index page
 	http.HandleFunc("/ordering-items-workflow.bpmn", handleOrderingItemsWorkflowBpmn) // the BPMN file, for documentation purpose
