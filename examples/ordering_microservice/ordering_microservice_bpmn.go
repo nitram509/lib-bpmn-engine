2f216d2dd113aef00a4c4082a386563f8334ff83
nitram509 <maki@bitkings.de>
2022-02-12 09:24:02 +0100

    fix quoting, part 2

--- examples/ordering_microservice/ordering_microservice_bpmn.go
+++ examples/ordering_microservice/ordering_microservice_bpmn.go
@@ -19,7 +19,7 @@ func initBpmnEngine() {
 
 func printHandler(job bpmn_engine.ActivatedJob) {
 	// do important stuff here
-	println(fmt.Sprintf("%s >>> Executing job '%'s", time.Now(), job.ElementId))
+	println(fmt.Sprintf("%s >>> Executing job '%s'", time.Now(), job.ElementId))
 	job.Complete()
 }
 
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/corbym/gocrest v1.0.5
 	github.com/senseyeio/duration v0.0.0-20180430131211-7c2a214ada46
+	github.com/antonmedv/expr v1.9.0
 )
