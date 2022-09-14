29eea8ebf242005680ab664c13cd75a1c22fdbd7
avery <305482686@qq.com>
2022-09-14 16:26:20 +0800

    add fun findActiveUserTasksForContinuation and test Test_user_tasks_can_be_continue

--- pkg/bpmn_engine/engine_usertasks_test.go
+++ pkg/bpmn_engine/engine_usertasks_test.go
@@ -20,3 +20,28 @@ func Test_user_tasks_can_be_handled(t *testing.T) {
 	then.AssertThat(t, instance.state, is.EqualTo(process_instance.COMPLETED))
 	then.AssertThat(t, cp.CallPath, is.EqualTo("user-task"))
 }
+
+func Test_user_tasks_can_be_continue(t *testing.T) {
+	// setup
+	bpmnEngine := New("name")
+	process, err := bpmnEngine.LoadFromFile("../../test-cases/simple-user-task.bpmn")
+	then.AssertThat(t, err, is.Nil())
+	cp := CallPath{}
+
+	instance, _ := bpmnEngine.CreateInstance(process.ProcessKey, nil)
+
+	userConfirm := false
+	bpmnEngine.AddTaskHandler("user-task", func(job ActivatedJob) {
+		if userConfirm {
+			cp.CallPathHandler(job)
+		}
+	})
+	bpmnEngine.RunOrContinueInstance(instance.instanceKey)
+
+	userConfirm = true
+
+	bpmnEngine.RunOrContinueInstance(instance.instanceKey)
+
+	then.AssertThat(t, instance.state, is.EqualTo(process_instance.COMPLETED))
+	then.AssertThat(t, cp.CallPath, is.EqualTo("user-task"))
+}
