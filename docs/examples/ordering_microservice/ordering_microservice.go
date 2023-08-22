adebdce77e7b56a231a8160ec6677db081b5b4d9
nitram509 <maki@bitkings.de>
2023-08-22 23:39:56 +0200

    clean example docs

--- docs/examples/ordering_microservice/ordering_microservice.go
+++ docs/examples/ordering_microservice/ordering_microservice.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "embed"
-	"encoding/json"
 	"github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine"
 	"net/http"
 )
@@ -10,35 +9,9 @@ import (
 var bpmnEngine bpmn_engine.BpmnEngineState
 var process *bpmn_engine.ProcessInfo
 
-type random struct {
-	Glossary struct {
-		Title    string `json:"title"`
-		GlossDiv struct {
-			Title     string `json:"title"`
-			GlossList struct {
-				GlossEntry struct {
-					ID        string `json:"ID"`
-					SortAs    string `json:"SortAs"`
-					GlossTerm string `json:"GlossTerm"`
-					Acronym   string `json:"Acronym"`
-					Abbrev    string `json:"Abbrev"`
-					GlossDef  struct {
-						Para         string   `json:"para"`
-						GlossSeeAlso []string `json:"GlossSeeAlso"`
-					} `json:"GlossDef"`
-					GlossSee string `json:"GlossSee"`
-				} `json:"GlossEntry"`
-			} `json:"GlossList"`
-		} `json:"GlossDiv"`
-	} `json:"glossary"`
-}
-
 // main does start a trivial microservice, listening on port 8080
 // open your web browser with http://localhost:8080/
 func main() {
-	r := random{}
-	json.Unmarshal([]byte(""), &r)
-
 	initHttpRoutes()
 	http.ListenAndServe(":8080", nil)
 }
