5165b574a9656cd8e5a975de25c4a2e995c8dc86
nitram509 <maki@bitkings.de>
2025-03-08 13:03:21 +0100

    update activity state diagram

--- pkg/bpmn_engine/actiivty.go
+++ pkg/bpmn_engine/actiivty.go
@@ -2,24 +2,47 @@ package bpmn_engine
 
 import "github.com/nitram509/lib-bpmn-engine/pkg/spec/BPMN20"
 
-// ActivityState as per BPMN 2.0 spec, section 13.2.2 Activity, page 428
-// State diagram (just partially shown):
+// ActivityState as per BPMN 2.0 spec, section 13.2.2 Activity, page 428, State diagram:
 //
-//	 ┌─────┐
-//	 │Ready│
-//	 └──┬──┘
-//	    |
-//	┌───▽──┐
-//	│Active│
-//	└───┬──┘
-//	    |
-//	┌───▽──────┐
-//	│Completing│
-//	└────┬─────┘
-//	     |
-//	┌────▽────┐
-//	│Completed│
-//	└─────────┘
+//	              (Inactive)
+//	                  O
+//	                  |
+//	A Token           v
+//	Arrives        ┌─────┐
+//	               │Ready│
+//	               └─────┘
+//	                  v         Activity Interrupted             An Alternative Path For
+//	                  O -------------------------------------->O----------------------------+
+//	Data InputSet     v                                        | Event Gateway Selected     |
+//	Available     ┌──────┐                         Interrupting|                            |
+//	              │Active│                         Event       |                            |
+//	              └──────┘                                     |                            v
+//	                  v         Activity Interrupted           v An Alternative Path For┌─────────┐
+//	                  O -------------------------------------->O ---------------------->│Withdrawn│
+//	Activity's work   v                                        | Event Gateway Selected └─────────┘
+//	completed     ┌──────────┐                     Interrupting|                            |
+//	              │Completing│                     Event       |                 The Process|
+//	              └──────────┘                                 |                 Ends       |
+//	                  v         Activity Interrupted           v  Non-Error                 |
+//	Completing        O -------------------------------------->O--------------+             |
+//	Requirements Done v                                  Error v              v             |
+//	Assignments   ┌─────────┐                              ┌───────┐       ┌───────────┐    |
+//	Completed     │Completed│                              │Failing│       │Terminating│    |
+//	              └─────────┘                              └───────┘       └───────────┘    |
+//	                  v  Compensation ┌────────────┐          v               v             |
+//	                  O ------------->│Compensating│          O <-------------O Terminating |
+//	                  |  Occurs       └────────────┘          v               v Requirements Done
+//	      The Process |         Compensation v   Compensation  |           ┌──────────┐     |
+//	      Ends        |       +--------------O----------------/|\--------->│Terminated│     |
+//	                  |       | Completes    |   Interrupted   |           └──────────┘     |
+//	                  |       v              |                 v              |             |
+//	                  | ┌───────────┐        |Compensation┌──────┐            |             |
+//	                  | │Compensated│        +----------->│Failed│            |             |
+//	                  | └─────┬─────┘         Failed      └──────┘            |             |
+//	                  |       |                               |               |             |
+//	                  v      / The Process Ends               / Process Ends /              |
+//	                  O<--------------------------------------------------------------------+
+//	             (Closed)
 type ActivityState string
 
 const (
