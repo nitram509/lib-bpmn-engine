76041f8dc0165e21ae17f889dccc81968fd31063
Amir Mousavi <amirmousavi0003@gmail.com>
2025-05-18 12:03:04 +0330

    Add Redis exporter implementation and update variable handling in BPMN engine

--- docs/examples/zeebe_exporter/zeebe_exporter.go
+++ docs/examples/zeebe_exporter/zeebe_exporter.go
@@ -3,23 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+
 	"github.com/hazelcast/hazelcast-go-client"
 	"github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine"
+	"github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine/exporter"
 	"github.com/nitram509/lib-bpmn-engine/pkg/bpmn_engine/exporter/zeebe"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	// create a new named engine
 	bpmnEngine := bpmn_engine.New()
-	// the exporter will require a running Hazelcast cluster at 127.0.0.1:5701
-	ctx := context.TODO()
-	config := hazelcast.Config{}
-	config.Cluster.Network.SetAddresses("localhost:5701")
-	client, err := hazelcast.StartNewClientWithConfig(ctx, config)
-	// create the client
-	exporter, _ := zeebe.NewExporterWithHazelcastClient(client)
+	// you can use hazelcast or redis as exporter
+	// hazelcastExporter, err := hazelcastExporter()
+	// if err != nil {
+	// 	panic("hazelcast exporter can't be created.")
+	// }
+	redisExporter, err := redisExporter()
+	if err != nil {
+		panic("redis exporter can't be created.")
+	}
 	// register the exporter
-	bpmnEngine.AddEventExporter(&exporter)
+	bpmnEngine.AddEventExporter(redisExporter)
 	// basic example loading a BPMN from file,
 	process, err := bpmnEngine.LoadFromFile("simple_task.bpmn")
 	if err != nil {
@@ -36,4 +41,33 @@ func main() {
 func printContextHandler(job bpmn_engine.ActivatedJob) {
 	// trivial handler is requires
 	job.Complete()
+}
+
+func redisExporter() (exporter.EventExporter, error) {
+	// the exporter will require a running Redis server at localhost:6379
+	redisExporter, err := zeebe.NewRedisExporter(redis.Options{
+		Addr:    "localhost:6379",
+		Network: "tcp",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &redisExporter, nil
+}
+
+func hazelcastExporter() (exporter.EventExporter, error) {
+	// the exporter will require a running Hazelcast cluster at 127.0.0.1:5701
+	ctx := context.TODO()
+	config := hazelcast.Config{}
+	config.Cluster.Network.SetAddresses("localhost:5701")
+	client, err := hazelcast.StartNewClientWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+	// create the exporter
+	exporter, err := zeebe.NewExporterWithHazelcastClient(client)
+	if err != nil {
+		return nil, err
+	}
+	return &exporter, nil
 }
